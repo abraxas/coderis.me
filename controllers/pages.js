53a41547728582a8c0a1b6df2eef4c2bf512c5be
C.Jay Martin <cjay.martin@gmail.com>
2014-02-08 01:13:45 -0500

    Grunt Jslint

--- controllers/pages.js
+++ controllers/pages.js
@@ -6,7 +6,6 @@ var admin_only = require("../lib/auth").admin_only;
 
 module.exports = function(app) {
     var model = new Page();
-
     var page_helper = function(p, req, res) {
         var model = {};
         model.active_tab = p;
@@ -17,9 +16,6 @@ module.exports = function(app) {
             res.render("index", model);
         });
     };
-
-
-
     app.get("/blog", function(req, res) {
         page_helper("blog", req, res);
     });
@@ -71,47 +67,45 @@ module.exports = function(app) {
             });
         });
     });
-
-    var view_page = function(req,res,err,page) {
-            res.format({
-                json: function() {
-                    if (err) {
-                        res.json({
-                            error: err
-                        });
-                    } else {
-                        res.json(page);
-                    }
-                },
-                html: function() {
-                    if (err) {
-                        res.send(500, err);
-                    } else {
-                        res.render("page", page);
-                    }
+    var view_page = function(req, res, err, page) {
+        res.format({
+            json: function() {
+                if (err) {
+                    res.json({
+                        error: err
+                    });
+                } else {
+                    res.json(page);
                 }
-            });
-    };
-    var get_page =  function(req, res) {
-        Page.findById(req.params.id, function(err, page) {
-            view_page(req,res,err,page);
+            },
+            html: function() {
+                if (err) {
+                    res.send(500, err);
+                } else {
+                    res.render("page", page);
+                }
+            }
         });
     };
-    app.get("/page/:id",get_page);
-
-    app.get(/^.*$/,function(req,res,next) {
-      var uri = req.path;
-      console.log("OORI = " + uri);
-      Page.findOne({route: uri},function(err,page) {
-          console.log("ERR IS: " + err);
-          console.log("PAGE IS: " + page);
-          if(!page) {
-             next(); 
-          }
-          else {
-              view_page(req,res,err,page);
-          }
-      });
+    var get_page = function(req, res) {
+        Page.findById(req.params.id, function(err, page) {
+            view_page(req, res, err, page);
+        });
+    };
+    app.get("/page/:id", get_page);
+    app.get(/^.*$/, function(req, res, next) {
+        var uri = req.path;
+        console.log("OORI = " + uri);
+        Page.findOne({
+            route: uri
+        }, function(err, page) {
+            console.log("ERR IS: " + err);
+            console.log("PAGE IS: " + page);
+            if (!page) {
+                next();
+            } else {
+                view_page(req, res, err, page);
+            }
+        });
     });
-    
 };
